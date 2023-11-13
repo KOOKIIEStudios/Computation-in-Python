2aa7978e0ddec5e35decddd3470e296533a14355
KOOKIIEStudios <25145447+KOOKIIEStudios@users.noreply.github.com>
2023-11-13 12:19:30 +0800

    FEAT: Allow list as input in float validator
    Signed-off-by: KOOKIIEStudios <25145447+KOOKIIEStudios@users.noreply.github.com>

--- assertions/common.py
+++ assertions/common.py
@@ -12,13 +12,37 @@ def validate_condition(
     print(error_message)
 
 
-def validate_floats(
+def validate_float(
         given: int | float,
         expected: int | float,
+) -> bool:
+    if isclose(given, expected, rel_tol=1e-5):
+        return True
+    return False
+
+
+def validate_floats(
+        given: int | float,
+        expected: int | float | list[int | float],
         error_message: str,
         success_message: str,
 ) -> None:
-    if isclose(given, expected, rel_tol=1e-5):
+    """Checks if the given value is close to the expected value
+    
+    Expected value can either be a single value or a list of values.
+    If a list is provided, any single element of the list being a match
+    will qualify as matching, and cause the success message to be printed.
+    """
+    is_correct = False
+
+    if isinstance(expected, list):    
+        for element in expected:
+            if validate_float(element):
+                is_correct = True
+    else:
+        is_correct = validate_float(expected)
+
+    if is_correct:
         print(success_message)
         return
     print(error_message)
